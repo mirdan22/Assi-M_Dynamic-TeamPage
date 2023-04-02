c1ac084a0b7849a41c830e404802388d69ee1498
mirdan22 <113371529+mirdan22@users.noreply.github.com>
2023-04-02 00:11:06 -0400

    team members
    bio added

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const popOutBtns = document.querySelectorAll('.pop-out-btn');
+
+const teamMembers = [
+  {
+    name: 'Member 1',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed nibh vitae nisi bibendum mollis.',
+  },
+  {
+    name: 'Member 2',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed nibh vitae nisi bibendum mollis.',
+  },
+  {
+    name: 'Member 3',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed nibh vitae nisi bibendum mollis.',
+  },
+];
